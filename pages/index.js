7df0296fd295e2575371fa8c93ff4cc8c02c836c
Lindsay Levine <lindslev.ll@gmail.com>
2021-03-24 08:24:36 -0400

    agane

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Next Image on Netlify</h1>
+        <h1 className={styles.title}>Next Image on Netlify!</h1>
 
         <p>
           <Image
